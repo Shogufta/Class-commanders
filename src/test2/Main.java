c16c49a84554a87fe5d0df941c632b0c1c473578
Shogufta <sh.hallimee@gmail.com>
2024-08-08 16:32:36 -0400

    Update Main.java

--- src/test2/Main.java
+++ src/test2/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
 
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
 
